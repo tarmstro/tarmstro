de5e391b391f671bd8b15d32ffaa4b84d7d165a3
Tom Armstrong <tarmstro@gmail.com>
2014-03-11 10:26:17 -0400

    moved extensions from handlebars to hbr

--- routes/publications.js
+++ routes/publications.js
@@ -7,6 +7,7 @@ exports.getPapers = function (req, res, next) {
 	mongo.Db.connect(mongoUri, function (err, db) {
 		db.collection('publications', function(err, collection){
 			collection.find().toArray(function(err, items) {
+				items.sort(function(x, y) { return parseInt(y.year) - parseInt(x.year); });
 				var context = {"papers": items};
 				console.log(context);
 				res.render('publist', context);
