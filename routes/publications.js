e7b8976beb2b7d24966799061a2d225ca3ee2ebf
Tom Armstrong <tarmstro@gmail.com>
2014-03-12 13:08:09 -0400

    removed extraneous functions for getting publications by id

--- routes/publications.js
+++ routes/publications.js
@@ -15,34 +15,3 @@ exports.getPapers = function (req, res, next) {
 		});
 	});
 };
-
-
-exports.getPaperByID = function (req, res) {
-	var id = req.params.id;
-	mongo.Db.connect(mongoUri, function (err, db) {
-		db.collection('publications', function(err, collection) {
-			BSON = mongo.BSONPure;
-			collection.findOne({"_id": new BSON.ObjectID(id)}, function(err, item) {
-				console.log(item);
-				res.render('publications', item);
-			});
-		});
-	});
-};
-
-
-// Just for testing purposes...then get rid of this.
-var populateDB = function() {
-	fs.readFile("public/data/pubs.json", function(err, f){
-		var papers = JSON.parse(f.toString());
-		mongo.Db.connect(mongoUri, function (err, db) {
-			db.collection('publications', function(err, collection){
-				collection.remove({}, {}, function(err, result) {});
-				collection.insert(papers, {safe: true}, function(err, result) {});
-  			});
-		}); 
-	}); 
-};
-
-
-populateDB();
--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 // web.js
 var express = require("express");
 	logfmt = require("logfmt");
-	ejs = require("ejs");
 	fs  = require("fs");
 	hbs = require("express3-handlebars").create();
 	publications = require("./routes/publications.js");
@@ -22,14 +21,12 @@ app.configure(function(){
 	app.use("/", express.static(__dirname+"/public"));
 	app.engine('hbs', hbs.engine);
 	app.set('view engine', 'hbs');
-	app.engine('.html', require('ejs').__express);
 	// add other things to serve here
 });
 
 
 app.get('/', function (req, res, next) { res.render('index',{}); });
 app.get('/publications/', publications.getPapers);
-app.get('/publications/:id', publications.getPaperByID);
 app.use(function(req, res) { res.send('404: Page not Found', 404); });
 
 
